68429a7ea25af1131586881f9c83171612f58fd7
Andrew Monshizadeh <me@amonshiz.com>
2024-12-01 19:55:07 -0500

    Reorg to have separate subcommands per day and part

---
+++ 2024/Sources/aoc2024/AOC2024.swift
@@ -0,0 +1,12 @@
+import ArgumentParser
+import Foundation
+
+@main
+struct AOC2024: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Solutions for Advent of Code 2024",
+        subcommands: [
+            Day1.self
+        ]
+    )
+}
---
+++ 2024/Sources/aoc2024/Day1.swift
@@ -0,0 +1,79 @@
+import ArgumentParser
+import Foundation
+import Parsing
+
+struct Day1: ParsableCommand {
+    struct Options: ParsableArguments {
+        @Option(
+            name: [.short, .customLong("input")],
+            help: "Path to the input file",
+            transform: URL.init(fileURLWithPath:)
+        )
+        var inputPath: URL
+    }
+
+    struct IDPair {
+        let leftID: Int
+        let rightID: Int
+    }
+
+    struct IDPairParser: Parser {
+        var body: some Parser<Substring, IDPair> {
+            Parse(IDPair.init) {
+                Int.parser()
+                Whitespace()
+                Int.parser()
+            }
+        }
+    }
+
+    struct IDPairsParser: Parser {
+        var body: some Parser<Substring, [IDPair]> {
+            Many {
+                IDPairParser()
+            } separator: {
+                "\n"
+            }
+        }
+    }
+
+    static let configuration = CommandConfiguration(
+        commandName: "day1",
+        abstract: "Solutions for Day 1",
+        subcommands: [
+            Part1.self,
+            // Part2.self
+        ]
+    )
+    
+    // Nested Part1 command
+    struct Part1: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Day 1 - Part 1 solution"
+        )
+
+        @OptionGroup var options: Options
+        
+        func validate() throws {
+            guard FileManager.default.fileExists(atPath: options.inputPath.path()) else {
+                throw ValidationError("File does not exist at \(options.inputPath.path())")
+            }
+        }
+        
+        func run() throws {
+            let pairsContent = try String(contentsOf: options.inputPath, encoding: .utf8)
+            let pairs = try IDPairsParser().parse(pairsContent)
+            let (left, right) = pairs.reduce(into: ([Int](), [Int]())) { acc, next in
+                acc.0.append(next.leftID)
+                acc.1.append(next.rightID)
+            }
+            let sortedLeft = left.sorted()
+            let sortedRight = right.sorted()
+            let result = zip(sortedLeft, sortedRight).reduce(0) { acc, next in
+                return acc + abs(next.0 - next.1)
+            }
+
+            print("result", result)
+        }
+    }
+} 
