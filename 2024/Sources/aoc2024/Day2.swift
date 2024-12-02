64a7083a84ce629343b397c794b9cf9a4f988b68
Andrew Monshizadeh <me@amonshiz.com>
2024-12-02 16:19:34 -0500

    Add part 2

--- 2024/Sources/aoc2024/Day2.swift
+++ 2024/Sources/aoc2024/Day2.swift
@@ -2,28 +2,59 @@ import Foundation
 import ArgumentParser
 import Parsing
 
+extension Array where Element == Int {
+    func isSafeLevels() -> Bool {
+        guard count > 1 else {
+            return true
+        }
+
+        let first = self[0]
+        let second = self[1]
+
+        let toCheck = first < second ? self : self.reversed()
+        for (current, next) in zip(toCheck, toCheck.dropFirst()) {
+            let diff = next - current
+            guard 1 <= diff, diff <= 3 else {
+                return false
+            }
+        }
+        return true
+    }
+}
+
 struct Day2: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Day 2 solution",
+        subcommands: [Part1.self, Part2.self]
+    )
+
     struct Report {
         let levels: [Int]
 
         var isSafe: Bool {
+            return levels.isSafeLevels()
+        }
+
+        var isSafe2: Bool {
             guard levels.count > 1 else {
                 return true
             }
 
-            let first = levels[0]
-            let second = levels[1]
+            func fixedLevels(excluding: Int) -> [Int] {
+                return Array(levels.enumerated().filter { $0.offset != excluding }.map { $0.element })
+            }
 
-            let toCheck = first < second ? levels : levels.reversed()
-            for (current, next) in zip(toCheck, toCheck.dropFirst()) {
-                let diff = next - current
-                guard 1 <= diff, diff <= 3 else {
-                    // print("not safe", self)
-                    return false
+            guard !levels.isSafeLevels() else {
+                return true
+            }
+
+            for i in 0..<levels.count {
+                guard !fixedLevels(excluding: i).isSafeLevels() else {
+                    return true
                 }
             }
-            print("safe", self)
-            return true
+
+            return false
         }
     }
 
@@ -48,18 +79,42 @@ struct Day2: ParsableCommand {
         }
     }
 
-    @Option(
-        name: [.customLong("input")],
-        help: "Path to the input file",
-        transform: URL.init(fileURLWithPath:)
-    )
-    var input: URL
+    struct Options: ParsableArguments {
+        @Option(
+            name: [.customLong("input")],
+            help: "Path to the input file",
+            transform: URL.init(fileURLWithPath:)
+        )
+        var input: URL
+    }
 
-    func run() throws {
-        let input = try String(contentsOf: input, encoding: .utf8)
-        let reports = try ReportsParser().parse(input)
-        let safeReports = reports.filter { $0.levels.count > 0 && $0.isSafe }
-        print(safeReports)
-        print(safeReports.count)
+    struct Part1: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Day 2 - Part 1 solution"
+        )
+
+        @OptionGroup var options: Options
+
+        func run() throws {
+            let input = try String(contentsOf: options.input, encoding: .utf8)
+            let reports = try ReportsParser().parse(input)
+            let safeReports = reports.filter { $0.levels.count > 0 && $0.isSafe }
+            print(safeReports.count)
+        }
+    }
+
+    struct Part2: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Day 2 - Part 2 solution"
+        )
+
+        @OptionGroup var options: Options
+
+        func run() throws {
+            let input = try String(contentsOf: options.input, encoding: .utf8)
+            let reports = try ReportsParser().parse(input)
+            let safeReports = reports.filter { $0.levels.count > 0 && $0.isSafe2 }
+            print(safeReports.count)
+        }
     }
 }
