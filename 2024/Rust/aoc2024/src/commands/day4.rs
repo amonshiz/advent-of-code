5ae780ce7d2b8f5d00b0fc3aa863230c0f40dac2
Andrew Monshizadeh <me@amonshiz.com>
2024-12-06 14:23:55 -0500

    Rust Day4 part 2

--- 2024/Rust/aoc2024/src/commands/day4.rs
+++ 2024/Rust/aoc2024/src/commands/day4.rs
@@ -7,6 +7,7 @@ pub fn handle(input_file: std::path::PathBuf, part_number: u8) -> Result<(), io:
 
     match part_number {
         1 => part1(&contents),
+        2 => part2(&contents),
         _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid part number")),
     }
 }
@@ -114,6 +115,40 @@ fn part1(contents: &str) -> Result<(), io::Error> {
         }
     }
 
+    println!("{}", count);
+    Ok(())
+}
+
+fn x_mases_beginning_at_line_and_character(lines: &Vec<Vec<char>>, line: usize, character: usize) -> usize {
+    if line < 1 || line == lines.len() - 1 || character < 1 || character == lines[line].len() - 1 {
+        return 0;
+    }
+
+    let left_up = collect_characters_beginning_at_line_and_character(lines, line - 1, character - 1, 3, &Direction::RightDown);
+    let left_down = collect_characters_beginning_at_line_and_character(lines, line + 1, character - 1, 3, &Direction::RightUp);
+    let right_up = collect_characters_beginning_at_line_and_character(lines, line - 1, character + 1, 3, &Direction::LeftDown);
+    let right_down = collect_characters_beginning_at_line_and_character(lines, line + 1, character + 1, 3, &Direction::LeftUp);
+    let mut count = 0;
+    let forward_mas = vec!['M', 'A', 'S'];
+    let reverse_mas = forward_mas.iter().rev().cloned().collect::<Vec<char>>();
+    if [left_up, left_down, right_up, right_down].iter().all(|direction| direction.eq(&forward_mas) || direction.eq(&reverse_mas)) {
+        count += 1;
+    }
+    return count;
+}
+
+fn part2(contents: &str) -> Result<(), io::Error> {
+    let lines: Vec<Vec<char>> = contents
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let mut count = 0;
+    for line in 0..lines.len() {
+        for character in 0..lines[line].len() {
+            count += x_mases_beginning_at_line_and_character(&lines, line, character);
+        }
+    }
     println!("{}", count);
     Ok(())
 }
