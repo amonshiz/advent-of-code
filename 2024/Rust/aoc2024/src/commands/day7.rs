27aa97cc710221d812524590a312025662e1e48e
Andrew Monshizadeh <me@amonshiz.com>
2024-12-07 08:08:19 -0500

    Rust Day7 part 2

--- 2024/Rust/aoc2024/src/commands/day7.rs
+++ 2024/Rust/aoc2024/src/commands/day7.rs
@@ -9,6 +9,31 @@ use nom::{
     IResult,
 };
 
+pub fn handle(input_file: std::path::PathBuf, part_number: u8) -> Result<(), io::Error> {
+    let contents = read_to_string(input_file)?;
+    match part_number {
+        1 => part_1(&contents),
+        2 => part_2(&contents),
+        _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid part number")),
+    }
+}
+
+enum Operation {
+    Addition,
+    Multiplication,
+    Concatenation,
+}
+
+impl Operation {
+    fn apply(&self, lhs: i64, rhs: i64) -> i64 {
+        match self {
+            Operation::Addition => lhs + rhs,
+            Operation::Multiplication => lhs * rhs,
+            Operation::Concatenation => (lhs.to_string() + &rhs.to_string()).parse::<i64>().unwrap(),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 struct Equation {
     test_value: i64,
@@ -16,8 +41,8 @@ struct Equation {
 }
 
 impl Equation {
-    fn is_valid(&self) -> bool {
-        fn check_factors(factors: &Vec<i64>, test_value: i64) -> bool {
+    fn is_valid(&self, operations: &[Operation]) -> bool {
+        fn check_factors(factors: &Vec<i64>, test_value: i64, operations: &[Operation]) -> bool {
             if factors.len() == 0 {
                 return false;
             }
@@ -30,15 +55,16 @@ impl Equation {
             // check combining the first two factors via multiplication
             let mut tail = factors.clone();
             let head = tail.drain(0..2).collect::<Vec<i64>>();
-            let mut addition_factors = [head[0] + head[1]].to_vec();
-            addition_factors.extend(&mut tail.iter().cloned());
-            let addition = check_factors(&addition_factors, test_value);
-            let mut multiplication_factors = [head[0] * head[1]].to_vec();
-            multiplication_factors.extend(&mut tail.iter().cloned());
-            let multiplication = check_factors(&multiplication_factors, test_value);
-            addition || multiplication
+            for operation in operations {
+                let mut updated_factors = [operation.apply(head[0], head[1])].to_vec();
+                updated_factors.extend(&mut tail.iter().cloned());
+                if check_factors(&updated_factors, test_value, operations) {
+                    return true;
+                }
+            }
+            false
         }
-        check_factors(&self.factors, self.test_value)
+        check_factors(&self.factors, self.test_value, operations)
     }
 }
 
@@ -59,17 +85,18 @@ fn parse_equations(input: &str) -> Vec<Equation> {
     separated_list1(newline, parse_equation)(input).unwrap().1
 }
 
-pub fn handle(input_file: std::path::PathBuf, part_number: u8) -> Result<(), io::Error> {
-    let contents = read_to_string(input_file)?;
-    match part_number {
-        1 => part_1(&contents),
-        _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid part number")),
-    }
-}
-
 fn part_1(contents: &str) -> Result<(), io::Error> {
     let equations = parse_equations(contents);
-    let valid_equations = equations.iter().filter(|e| e.is_valid()).collect::<Vec<&Equation>>();
+    let valid_equations = equations.iter().filter(|e| e.is_valid(&[Operation::Addition, Operation::Multiplication])).collect::<Vec<&Equation>>();
+    println!("{}", valid_equations.len());
+    let sum_of_valid_equations = valid_equations.iter().fold(0, |acc, e| acc + e.test_value);
+    println!("{:?}", sum_of_valid_equations);
+    Ok(())
+}
+
+fn part_2(contents: &str) -> Result<(), io::Error> {
+    let equations = parse_equations(contents);
+    let valid_equations = equations.iter().filter(|e| e.is_valid(&[Operation::Addition, Operation::Multiplication, Operation::Concatenation])).collect::<Vec<&Equation>>();
     println!("{}", valid_equations.len());
     let sum_of_valid_equations = valid_equations.iter().fold(0, |acc, e| acc + e.test_value);
     println!("{:?}", sum_of_valid_equations);
